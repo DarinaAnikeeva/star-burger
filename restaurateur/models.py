a8b427189bcb6068f25a69f6b00bc4cba2b6c089
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2023-01-27 20:30:16 +0300

    create Coordinates

--- restaurateur/models.py
+++ restaurateur/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Coordinates(models.Model):
+    address = models.CharField(
+        verbose_name='адрес',
+        max_length=200,
+    )
+    lon = models.FloatField(
+        verbose_name='Долгота',
+        blank=True,
+    )
+    lat = models.FloatField(
+        verbose_name='Широта',
+        blank=True,
+    )
+    class Meta:
+        verbose_name = 'координаты заказа'
+        verbose_name_plural = 'координаты заказа'
+
+    def __str__(self):
+        return self.address
