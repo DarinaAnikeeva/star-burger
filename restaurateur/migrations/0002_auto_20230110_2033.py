53712d25c1e8dc78f5fbb165f10337a6bf61fe99
DarinaAnikeeva <102724828+DarinaAnikeeva@users.noreply.github.com>
2023-01-27 20:30:47 +0300

    updates

---
+++ restaurateur/migrations/0002_auto_20230110_2033.py
@@ -0,0 +1,22 @@
+from restaurateur.models import Coordinates
+from django.db import migrations
+
+def coordinates(apps, schema_editor):
+    Coordinates_1 = apps.get_model('foodcartapp', 'Coordinates')
+    for coordinates in Coordinates_1.objects.all().iterator():
+        Coordinates.objects.create(
+            address=coordinates.address,
+            lon=coordinates.lon,
+            lat=coordinates.lat
+        )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('restaurateur', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(coordinates)
+    ]
